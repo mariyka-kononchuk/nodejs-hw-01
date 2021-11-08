b74c189315ed3ba0a2c7a62ebf4c75aa02145941
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-11-08 17:11:05 +0200

    path edited

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const contactsPath = path.resolve('./db', '/contacts.json')
+const contactsPath = path.resolve('./db/contacts.json')
 
 function listContacts() {
     fs.readFile(contactsPath);
